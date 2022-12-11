9793e1bedfc0f9a6d7e72d58ce7f282a9a701ae4
Suleyman Karaca <114638048+KaracaSuleyman@users.noreply.github.com>
2022-12-11 13:31:19 +0300

    Add files via upload

---
+++ ders3_06_12_2022_arrays/ders3_06_12_2022_arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ders3_06_12_2022_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] futbol_takimi = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            
+            for(int i=0; i<=futbol_takimi.Length-1; i++)
+            {
+                
+                Console.WriteLine(futbol_takimi[i]);
+            }
+            Console.WriteLine(Arra)
+            
+        }
+    }
+}
